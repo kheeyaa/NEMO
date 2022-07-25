61eba2909e243dee682e6dfaf5179830e878a4a0
Kang Hee <kheeyaa@icloud.com>
2022-07-25 16:38:23 +0900

    [feature] BasicLayout 컴포넌트 추가 #5

--- client/src/layout/BasicLayout.tsx
+++ client/src/layout/BasicLayout.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import Header from 'components/BasicLayout/Header';
+import Nav from 'components/BasicLayout/Nav';
 import { Outlet } from 'react-router-dom';
+import { css } from '@emotion/react';
+import { MOBILE_WIDTH } from 'const/style/size';
 
 function BasicLayout() {
   return (
-    <div>
-      <Outlet />
+    <div css={basicLayoutStyle}>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Nav />
     </div>
   );
 }
+
+const basicLayoutStyle = css`
+  max-width: ${MOBILE_WIDTH};
+  min-height: 100vh;
+  background-color: white;
+  margin: 0 auto;
+`;
 
 export default BasicLayout;
