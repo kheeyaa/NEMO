dc68144b17dea8bf81006c3299cd80dadb6014c1
Kang Hee <kheeyaa@icloud.com>
2022-07-25 16:36:45 +0900

    [style] global CSS 적용 #5

--- client/src/page/App.tsx
+++ client/src/page/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Global } from '@emotion/react';
+import reset from 'const/style/reset';
 import { Outlet } from 'react-router-dom';
+import 'assets/css/normalize.css';
 
 function App() {
   return (
     <div className="App">
-      NEMO
+      <Global styles={reset} />
       <Outlet />
     </div>
   );
