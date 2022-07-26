579e5734e8419335058af92217a832d41568c54a
Kang Hee <kheeyaa@icloud.com>
2022-07-26 10:13:18 +0900

    [style] basicLayout 스타일 수정 #5

--- client/src/layout/BasicLayout.tsx
+++ client/src/layout/BasicLayout.tsx
@@ -4,6 +4,7 @@ import Nav from 'components/BasicLayout/Nav';
 import { Outlet } from 'react-router-dom';
 import { css } from '@emotion/react';
 import { MOBILE_WIDTH } from 'const/style/size';
+import { pxToRem } from 'utils/style/pxToRem';
 
 function BasicLayout() {
   return (
@@ -22,6 +23,12 @@ const basicLayoutStyle = css`
   min-height: 100vh;
   background-color: white;
   margin: 0 auto;
+  margin-top: ${pxToRem(44)};
+  margin-bottom: ${pxToRem(64)};
+
+  main {
+    height: 300vh;
+  }
 `;
 
 export default BasicLayout;
