f2a833a956c8322635a4e9598b08caf24b14710d
Kang Hee <kheeyaa@icloud.com>
2022-07-27 14:04:03 +0900

    [feature] 버튼 컴포넌트 생성 #11

---
+++ client/src/components/common/Button.tsx
@@ -0,0 +1,100 @@
+import { css } from '@emotion/react';
+import { colors } from 'const/style/colors';
+import React, { useLayoutEffect, useState } from 'react';
+import { pxToRem } from 'utils/style/pxToRem';
+
+type ButtonType = 'Basic' | 'Fill';
+
+interface BasicButtonStyle {
+  backgroundColor?: string;
+  border?: string;
+  color?: string;
+}
+
+interface ButtonStyle {
+  width: string;
+  backgroundColor?: string;
+  border?: string;
+  color?: string;
+}
+
+interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    ButtonStyle {
+  buttonType?: ButtonType;
+}
+
+interface ButtonStylesType {
+  [key: string]: BasicButtonStyle;
+}
+
+const buttonStyles: ButtonStylesType = {
+  Basic: {
+    backgroundColor: 'white',
+    border: `1px solid ${colors.gray[200]}`,
+    color: colors.black[100],
+  },
+  Fill: {
+    backgroundColor: colors.yellow[100],
+    border: 'none',
+    color: 'white',
+  },
+};
+
+function Button({
+  width,
+  backgroundColor,
+  border,
+  color,
+  buttonType = 'Basic',
+  children,
+  ...restProps
+}: ButtonProps) {
+  const [style, setStyle] = useState({
+    width,
+    ...buttonStyles[buttonType],
+  });
+
+  useLayoutEffect(() => {
+    const customStyle: BasicButtonStyle = { backgroundColor, border, color };
+    let newStyle = {
+      ...style,
+    };
+    Object.entries(customStyle).forEach(([key, value]) => {
+      if (value) newStyle = { ...style, [key]: value };
+    });
+
+    setStyle({
+      backgroundColor,
+      border,
+      color,
+      ...newStyle,
+    });
+  }, [backgroundColor, border, color]);
+
+  return (
+    <button css={buttonStyle(style)} {...restProps}>
+      {children}
+    </button>
+  );
+}
+
+const buttonStyle = ({
+  width,
+  backgroundColor,
+  border,
+  color,
+}: ButtonStyle) => css`
+  width: ${width};
+  padding: ${pxToRem(15)};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: ${backgroundColor};
+  color: ${color};
+  border: ${border};
+  border-radius: ${pxToRem(4)};
+`;
+
+export default Button;
