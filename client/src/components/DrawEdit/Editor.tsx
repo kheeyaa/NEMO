e3647ed8759d36d2b095062927a7c0a642e078c8
Kang Hee <kheeyaa@icloud.com>
2022-08-12 10:44:43 +0900

    [feature] 캔버스 기본 배경색 추가, 주석 추가 #14

--- client/src/components/DrawEdit/Editor.tsx
+++ client/src/components/DrawEdit/Editor.tsx
@@ -49,17 +49,29 @@ function Editor() {
     return result;
   };
 
+  /** 캔버스 크기, 유닛 크기 설정 */
   useLayoutEffect(() => {
     const newSize = getMainUnitWidth();
     const newUnit = newSize / CANVAS_SIZE;
+
     setSize(newSize);
     setUnit(newUnit);
   }, []);
 
+  /** 캔버스 기본 배경색 화이트로 설정 */
+  useEffect(() => {
+    if (context) {
+      context.fillStyle = '#fff';
+      context.fillRect(0, 0, size, size);
+    }
+  }, [context, size]);
+
+  /** 브러쉬 굵기 설정 */
   useLayoutEffect(() => {
     setLineWidth(unit * lineWidthStep);
   }, [unit, lineWidthStep]);
 
+  /** 좌표 구함 */
   const getCoordinates = (event: MouseEvent): Coordinate | undefined => {
     if (!canvasRef.current) {
       return;
@@ -72,16 +84,18 @@ function Editor() {
     };
   };
 
+  /** 선 그리기 */
   const drawLine = (originalMousePosition: Coordinate) => {
     if (!context) return;
 
-    const pixelX = getUnitPoition(originalMousePosition.x);
-    const pixelY = getUnitPoition(originalMousePosition.y);
+    const unitPixelX = getUnitPoition(originalMousePosition.x);
+    const unitPixelY = getUnitPoition(originalMousePosition.y);
 
     context.fillStyle = action === '브러쉬' ? color : BACK_GROUND;
-    context.fillRect(pixelX, pixelY, lineWidth, lineWidth);
+    context.fillRect(unitPixelX, unitPixelY, lineWidth, lineWidth);
   };
 
+  /** 그림 그리기 시작 (isPainting, mousePosition) */
   const startPaint = useCallback(
     (event: MouseEvent) => {
       if (!(action === '브러쉬' || action === '지우개')) return;
@@ -95,6 +109,7 @@ function Editor() {
     [action],
   );
 
+  /** 그림 그림 (선 그리고, 다음 포지션 설정) */
   const paint = useCallback(
     (event: MouseEvent) => {
       if (!isPainting) return;
@@ -110,6 +125,7 @@ function Editor() {
     [isPainting, mousePosition],
   );
 
+  /** 그림 멈춤 (브러쉬, 지우개면 마지막 점 찍음) */
   const stopPaint = useCallback(() => {
     if (!isPainting) return;
     if (!(action === '브러쉬' || action === '지우개')) return;
@@ -118,6 +134,7 @@ function Editor() {
     setIsPainting(false);
   }, [mousePosition, action]);
 
+  /** 색상 값 꺼냄 */
   const pickColor = useCallback(
     (event: MouseEvent) => {
       if (action !== '스포이드') return;
@@ -132,6 +149,7 @@ function Editor() {
     [action],
   );
 
+  /** 캔버스 이벤트 핸들러 등록 */
   useEffect(() => {
     if (!canvasRef.current) {
       return;
